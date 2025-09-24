851300f011325a981011ac98f94f8f48dbac0ba9
Maks <lisichenkowork@gmail.com>
2025-09-24 15:28:18 +0200

    Initial commit

---
+++ src/main/java/mate/academy/bookstoreappspring/BookStoreAppSpringApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.bookstoreappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/bookstoreappspring/BookStoreAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.bookstoreappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
