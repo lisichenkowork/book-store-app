b41b3ad3f0ded299db39ab586959d302bde07b7c
Maks <lisichenkowork@gmail.com>
2025-10-08 09:22:06 +0200

    added GlobalHandlerException

--- src/main/java/mate/academy/bookstoreappspring/dto/BookCreateRequestDto.java
+++ src/main/java/mate/academy/bookstoreappspring/dto/BookCreateRequestDto.java
@@ -1,5 +1,9 @@
 package mate.academy.bookstoreappspring.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -7,11 +11,19 @@ import java.math.BigDecimal;
 @Data
 public class BookCreateRequestDto {
 
+    @NotBlank
     private String title;
+    @NotBlank
     private String author;
+    @NotBlank
     private String isbn;
+    @NotBlank
+    @DecimalMin("0.0")
     private BigDecimal price;
+    @NotBlank
     private String description;
+    @NotBlank
     private String coverImage;
+
     private boolean isDeleted = false;
 }
